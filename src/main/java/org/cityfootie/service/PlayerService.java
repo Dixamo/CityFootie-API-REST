8e6f1bc80e36f4fce8e4c00cffdf93adde5be212
sebasgr10 <sebasgrgr10@gmail.com>
2023-05-17 20:28:30 +0200

    wasiii

--- src/main/java/org/cityfootie/service/PlayerService.java
+++ src/main/java/org/cityfootie/service/PlayerService.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 @Service
 public class PlayerService {
+    //WAAASIIIIIII
     @Autowired
     private PlayerDAO playerDAO;
     public Player loginPlayer(String email, String password) {
